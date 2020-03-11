40b1044974465bc209dae226c7fc8c0e61f6e145
kingjulian31405 <kingjulian314@gmail.com>
2020-03-11 19:00:49 +0000

    Add Procfile for heroku and create bot.py

---
+++ bot.py
@@ -0,0 +1,8 @@
+import os
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+Guild = os.getenv('DISCORD_GUILD')
+bot = commands.Bot(command_prefix='!')

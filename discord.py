13af43bc418055e4332d13f081b344e69ede357f
Denish20003 <54958490+Denish20003@users.noreply.github.com>
2020-03-20 15:49:52 -0400

    Code

--- discord.py
+++ discord.py
@@ -1,13 +1,29 @@
 import os
+import random
 
+import discord
 from discord.ext import commands 
 from dotenv import load_dotenv
+from discord.ext.commands import Bot                                                                                                                    
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
-async def ready():
-    print('I am here')
+@bot.command(name = 'casino')
+async def casino(ctx):
+    await ctx.send('1. Flip the coin')
+    await ctx.send('2. Roll the dice')
+    await ctx.send("which number game do you want to play e.g:!1 " )
+
+@bot.command(name = '1')
+async def coin_flip(ctx):
+    await ctx.send(random.choice(['Head','Tail']))
+
+@bot.command(name = '2')
+async def dice_roll(ctx):
+    await ctx.send(random.randint(1,7))
+
+bot.run(TOKEN)
 
 

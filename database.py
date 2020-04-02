5751abee71ce7f8c0b18fe15b8987f6e6864330f
Lauren-Coste <54964221+Lauren-Coste@users.noreply.github.com>
2020-04-02 18:38:29 -0400

    Update database.py

--- database.py
+++ database.py
@@ -4,47 +4,47 @@ import discord
 
 bot = commands.Bot('!')
 
-amounts = {}
+users = {}
 
 @bot.event
 async def on_ready():
-    global amounts
+    global users
     try:
-        with open('amounts.json') as f:
-            amounts = json.load(f)
+        with open('users.json') as f:
+            users = json.load(f)
     except FileNotFoundError:
-        print("Could not load amounts.json")
-        amounts = {}
+        print("Could not load users.json")
+        users = {}
 
 @bot.command(name = 'check')
 async def balance(ctx):
-    id = ctx.message.author.id
-    if id in amounts:
-        await ctx.send("You have {} in your account".format(amounts[ctx.message.author.id]))
+    id = ctx.message.member.user.id
+    if id in users:
+        await ctx.send("You have {} in your account".format(users[ctx.message.member.user.id]))
     else:
         await ctx.send("Created a user first")
 
 @bot.command(name = 'register')
 async def register(ctx):
-    id = ctx.message.author.id
-    if id not in amounts:
-        amounts[id] = 100
+    id = ctx.message.member.user.id
+    if id not in users:
+        users[id] = 100
         await ctx.send("You are now registered")
     else:
         await ctx.send("You already have an account")
 
 @bot.command(name = 'accounts')
 async def active(ctx):
-        await ctx.send(amounts)
+        await ctx.send(users)
 
 @bot.command(name = 'save')
 async def save(ctx):
-    with open('amounts.json', 'w+') as f:
-        json.dump(amounts, f)
+    with open('users.json', 'w+') as f:
+        json.dump(users, f)
     await ctx.send("everything was save")
     
     
-@bot.command(name = 'game')
+@bot.command(name = 'up')
 async def testing(ctx):
     users[id] += 2
     await ctx.send("amount save")

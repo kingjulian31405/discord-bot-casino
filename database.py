5595b9f2c35d3d23e83c72f5aac9a9c891e3cc95
Lauren-Coste <54964221+Lauren-Coste@users.noreply.github.com>
2020-04-06 21:36:03 -0400

    Improve Code - Added gaining and Losing System

--- database.py
+++ database.py
@@ -1,52 +1,64 @@
 from discord.ext import commands
 import json
 import discord
-
+import os
+ 
 bot = commands.Bot('!')
-
-users = {}
-
+ 
 @bot.event
 async def on_ready():
     global users
-    try:
-        with open('users.json') as f:
+    if os.path.exists('users.json'):
+        with open('users.json', 'r') as f:
             users = json.load(f)
-    except FileNotFoundError:
-        print("Could not load users.json")
+        print("Everything is ready")
+    else:
         users = {}
-
+        print("Everything is ready")
+ 
 @bot.command(name = 'check')
 async def balance(ctx):
-    id = ctx.message.author.name
-    if id in users:
-        await ctx.send("You have {} in your account".format(users[ctx.message.author.name]))
+    ID = ctx.message.author.name
+    if ID in users:
+        await ctx.send("You have {} in your account".format(users[ID]))
     else:
         await ctx.send("Register first")
-
+ 
 @bot.command(name = 'register')
 async def register(ctx):
-    id = ctx.message.author.name
-    if id not in users:
-        users[id] = 100
+    ID = ctx.message.author.name
+    if ID not in users:
+        users[ID] = 100
         await ctx.send("You are now registered")
     else:
         await ctx.send("You already have an account")
-
+ 
 @bot.command(name = 'accounts')
 async def active(ctx):
         await ctx.send(users)
-
+ 
 @bot.command(name = 'save')
 async def save(ctx):
     with open('users.json', 'w+') as f:
         json.dump(users, f)
     await ctx.send("everything was save")
     
-    
-@bot.command(name = 'up')
-async def testing(ctx):
-    users[id] += 2
-    await ctx.send("up")
+ 
+@bot.command(name = 'losed')
+async def subtract(ctx):
+    ID = ctx.message.author.name
+    users[ID] -= 10
+    with open('users.json', 'w+') as f:
+        json.dump(users, f)
+    await ctx.send("Money taken away")
+ 
+@bot.command(name = 'gain')
+async def add(ctx):
+    ID = ctx.message.author.name
+    amount = users[ID] + 50
+    with open('users.json', 'w+') as f:
+        users[ID] = amount
+        json.dump(users, f)
+    await ctx.send("Money added")
 
-bot.run("NjkwNzAwMjQzNDMxNDU2ODM4.XoPMjg.CvJPW5q2dEiYvFL3o69lbyfdlos")
+bot.run("BOT-TOKEN")
